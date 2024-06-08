e29e345d1cd126fb8d020b2ad68ad998debeca90
framirez-lab <fabian.ramirezv@gmail.com>
2024-06-08 03:43:00 -0400

    Se agregan archivos

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* const select1 = document.getElementById('select1')
+const select2 = document.getElementById('select2')
+const select3 = document.getElementById('select3')
+
+const total = select1 + select2 + select3
+
+if (total > 10) {
+    alert("ok")
+} else {
+    alert("llevas", total)
+} */
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Desafío 2 Javascript: Condiciones
+## Fecha: 08-06-2024
+### Revisa demo https://framirez-lab.github.io/condiciones.github.io/
